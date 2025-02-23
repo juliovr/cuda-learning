be38c8d1c7f330286203c4bf06bfe87a0aaecaf1
Julio Vidaurre <julio.vidaurre@gmail.com>
2025-02-22 22:29:12 -0300

    Blur image: normal and CUDA versions

--- 03_blur_image/main.cu
+++ 03_blur_image/main.cu
@@ -32,26 +32,82 @@ typedef float f32;
 
 
 #define CHANNELS 3
+#define BLUR_SIZE 10
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 
 __global__
-void color_to_grayscale_conversion(u8 *p_out, u8 *p_in, int width, int height)
+void blur_kernel(u8 *out, u8 *in, int width, int height)
 {
     int col = blockIdx.x*blockDim.x + threadIdx.x;
     int row = blockIdx.y*blockDim.y + threadIdx.y;
     if (col < width && row < height) {
-        // Get 1D offset for the grayscale image (output)
-        int gray_offset = row*width + col;
+        int pix_val_r = 0;
+        int pix_val_g = 0;
+        int pix_val_b = 0;
+        int pixels = 0;
 
-        int rgb_offset = gray_offset*CHANNELS;
-        u8 r = p_in[rgb_offset + 0];
-        u8 g = p_in[rgb_offset + 1];
-        u8 b = p_in[rgb_offset + 2];
+        // Get the average of the surroundings BLUR_SIZE x BLUR_SIZE box.
+        for (int blur_row = -BLUR_SIZE; blur_row < BLUR_SIZE + 1; ++blur_row) {
+            for (int blur_col = -BLUR_SIZE; blur_col < BLUR_SIZE + 1; ++blur_col) {
+                int cur_row = row + blur_row;
+                int cur_col = col + blur_col;
 
-        p_out[gray_offset] = 0.21f*r + 0.71f*g + 0.07f*b;
+                if (cur_row >= 0 && cur_row < height &&
+                    cur_col >= 0 && cur_col < width)
+                {
+                    int offset = (cur_row*width + cur_col)*CHANNELS;
+                    pix_val_r += in[offset + 0];
+                    pix_val_g += in[offset + 1];
+                    pix_val_b += in[offset + 2];
+
+                    ++pixels; // Keep track of number of pixels in the avg.
+                }
+            }
+        }
+
+        int offset = (row*width + col)*CHANNELS;
+        out[offset + 2] = (u8)((f32)pix_val_r / (f32)pixels);
+        out[offset + 1] = (u8)((f32)pix_val_g / (f32)pixels);
+        out[offset + 0] = (u8)((f32)pix_val_b / (f32)pixels);
+    }
+}
+
+void blur(u8 *out, u8 *in, int width, int height)
+{
+    for (int row = 0; row < height; ++row) {
+        for (int col = 0; col < width; ++col) {
+            int pix_val_r = 0;
+            int pix_val_g = 0;
+            int pix_val_b = 0;
+            int pixels = 0;
+
+            // Get the average of the surroundings BLUR_SIZE x BLUR_SIZE box.
+            for (int blur_row = -BLUR_SIZE; blur_row < BLUR_SIZE + 1; ++blur_row) {
+                for (int blur_col = -BLUR_SIZE; blur_col < BLUR_SIZE + 1; ++blur_col) {
+                    int cur_row = row + blur_row;
+                    int cur_col = col + blur_col;
+
+                    if (cur_row >= 0 && cur_row < height &&
+                        cur_col >= 0 && cur_col < width)
+                    {
+                        int offset = (cur_row*width + cur_col)*CHANNELS;
+                        pix_val_r += in[offset + 0];
+                        pix_val_g += in[offset + 1];
+                        pix_val_b += in[offset + 2];
+
+                        ++pixels; // Keep track of number of pixels in the avg.
+                    }
+                }
+            }
+
+            int offset = (row*width + col)*CHANNELS;
+            out[offset + 2] = (u32)((f32)pix_val_r / (f32)pixels);
+            out[offset + 1] = (u32)((f32)pix_val_g / (f32)pixels);
+            out[offset + 0] = (u32)((f32)pix_val_b / (f32)pixels);
+        }
     }
 }
 
@@ -85,22 +141,6 @@ struct Bitmap {
 };
 #pragma pack(pop)
 
-
-#define COLOR_TABLE_SIZE  (256)
-static u32 color_table[COLOR_TABLE_SIZE];
-
-static void fill_color_table()
-{
-    for (int i = 0; i < COLOR_TABLE_SIZE; i++) {
-        u8 r = i;
-        u8 g = i;
-        u8 b = i;
-        u8 reserved = 0;
-
-        color_table[i] = (reserved << 24) | (b << 16) | (g << 8) | (r << 0);
-    }
-}
-
 void save_to_bitmap(const char *output_filename, u8 *image, int x, int y, int channels_in_file)
 {
     s32 width = x;
@@ -120,34 +160,17 @@ void save_to_bitmap(const char *output_filename, u8 *image, int x, int y, int ch
     info_header.bits_per_pixel = channels_in_file*8;
     info_header.compression = 0;
     info_header.image_size = image_size_bytes;
-    info_header.colors_used = COLOR_TABLE_SIZE;
+    info_header.colors_used = 0;
     info_header.important_colors = 0;
 
     Bitmap bitmap = {};
     bitmap.header = header;
     bitmap.info_header = info_header;
 
-#if 0
-    FILE *reference = fopen("reference.bmp", "rb");
-    Bitmap *reference_bitmap;
-    if (reference) {
-        fseek(reference, 0, SEEK_END);
-        int size = ftell(reference);
-        fseek(reference, 0, SEEK_SET);
-
-        u8 *memory = (u8 *)malloc(size);
-        fread(memory, size, 1, reference);
-
-        fclose(reference);
-
-        reference_bitmap = (Bitmap *)memory;
-    }
-#endif
 
     FILE *output = fopen(output_filename, "wb");
     if (output) {
         fwrite(&bitmap, sizeof(Bitmap), 1, output);
-        fwrite(color_table, COLOR_TABLE_SIZE * sizeof(u32), 1, output);
         fwrite(image, image_size_bytes, 1, output);
     }
 
@@ -157,8 +180,6 @@ void save_to_bitmap(const char *output_filename, u8 *image, int x, int y, int ch
 
 int main()
 {
-    fill_color_table();
-
     int width;
     int height;
     int channels_in_file;
@@ -173,39 +194,40 @@ int main()
 #if 1
     cudaError_t err = cudaSuccess;
 
-    int source_image_size = width*height*CHANNELS;
-    int dest_image_size = width*height;
-
-    u8 *dest_image = (u8 *)malloc(dest_image_size);
+    int image_size = width*height*CHANNELS;
+    
+    u8 *dest_image = (u8 *)malloc(image_size);
     
     u8 *source_image_d;
     u8 *dest_image_d;
 
-    cudaMalloc((void **)&source_image_d, source_image_size);
-    cudaMalloc((void **)&dest_image_d, dest_image_size);
+    cudaMalloc((void **)&source_image_d, image_size);
+    cudaMalloc((void **)&dest_image_d, image_size);
 
-    cudaMemcpy(source_image_d, image, source_image_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(source_image_d, image, image_size, cudaMemcpyHostToDevice);
 
     dim3 dim_grid(ceil(width / 32.0f), ceil(height / 32.0f), 1);
     dim3 dim_block(32, 32, 1);
-    color_to_grayscale_conversion<<<dim_grid, dim_block>>>(dest_image_d, source_image_d, width, height);
+    blur_kernel<<<dim_grid, dim_block>>>(dest_image_d, source_image_d, width, height);
 
-    cudaMemcpy(dest_image, dest_image_d, dest_image_size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(dest_image, dest_image_d, image_size, cudaMemcpyDeviceToHost);
 
     cudaFree(source_image_d);
     cudaFree(dest_image_d);
 
-    save_to_bitmap("grayscale_gpu.bmp", dest_image, width, height, 1);
+    save_to_bitmap("blur.bmp", dest_image, width, height, CHANNELS);
 
     printf("Done\n");
 
 #else
-    u8 *image_grayscale = color_to_grayscale_cpu(image, x, y, CHANNELS);
-    if (image_grayscale) {
-        save_to_bitmap("grayscale.bmp", image_grayscale, x, y, 1);
-        printf("Done\n");
-        free(image_grayscale);
-    }
+    int image_size = width*height*CHANNELS;
+    u8 *dest_image = (u8 *)malloc(image_size);
+
+    blur(dest_image, image, width, height);
+
+    save_to_bitmap("blur_cpu.bmp", dest_image, width, height, CHANNELS);
+
+    printf("Done\n");
 #endif
 
     stbi_image_free(image);
